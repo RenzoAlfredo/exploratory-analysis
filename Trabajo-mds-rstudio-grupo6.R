bcf70990b87c3448a05b9060170ca2381ee8bd79
Renzo H <renzo.huamanyauri.h@uni.pe>
2023-08-01 12:26:19 -0500

    adiciono instalaciòn de paquetes

--- Trabajo-mds-rstudio-grupo6.R
+++ Trabajo-mds-rstudio-grupo6.R
@@ -3,7 +3,7 @@
 #                                      #
 #     ANALISIS EXPLORATORIO            #
 #     DE LA BASE MODELO SEGMENTACION Y #
-#     PRODECCION - CAJA                #
+#     PREDICCIÒN - CAJA                #
 #                                      #
 #              GRUPO 6                 #
 #       RENZO HUAMANYAURI HUAMAN       #
@@ -16,6 +16,85 @@
 rm(list = ls())
 
 #---------------------------------------------------------
+
+#-------------- 0. Cargando los paquetes --------------#
+
+install.packages(c(
+    "caret",
+    "DataExplorer",
+    "VIM",
+    "missForest",
+    "ggplot2",
+    "dummies",
+    "lattice",
+    "colorspace",
+    "data.table",
+    "randomForest",
+    "foreach",
+    "itertools",
+    "MASS",
+    "pROC",
+    "foreign",
+    "gmodels",
+    "InformationValue",
+    "caTools",
+    "MLmeTrics",
+    "dplyr",
+    "iterators",
+    "MLmeTrics",
+    "tidyverse",
+    "kableExtra",
+    "scales",
+    "Boruta",
+    "BRugs",
+    "R2OpenBUGS",
+    "factoextra",
+    "mvoutlier",
+    "outliers",
+    "cluster",
+    "fpc",
+    "mclust",
+    "dbscan",
+    "readxl",
+    "psych",
+    "corrplot",
+    "mclust",
+    "gclus",
+    "rrcov",
+    "tourr",
+    "aplpack",
+    "TeachingDemos",
+    "rgl",
+    "ape",
+    "DMwR",
+    "GGally",
+    "Hmisc",
+    "PerformanceAnalytics",
+    "e1071",
+    "class",
+    "sqldf",
+    "unbalanced",
+    "h2o",
+    "ROCR",
+    "tm",
+    "SnowballC",
+    "wordcloud",
+    "C50",
+    "e1071",
+    "ggpubr",
+    "gridExtra",
+    "gdata",
+    "ggbiplot",
+    "GPArotation",
+    "nFactors",
+    "gplots",
+    "RColorBrewer",
+    "semPlot"
+), dependencies = T)
+
+
+install.packages(c("tidyverse"), dependencies = T)
+
 
 #-------------- 1. Cargamos las librerìas --------------#
 library(ggplot2)
@@ -90,8 +169,8 @@ names(ds) # nombre de los campos
 dim(ds) # mostrar la dimensiòn de los datos
 dplyr::glimpse(ds)
 
-#Var continuas: Edad, CantCred, Desem, SalVigSol, Monto_Tasaciòn, DiasEfec, MaxDeuda, MaxDiasAtra, MaxGramOro, AntiDias
-#Var categòricas: Sexo, EstaCiv, TipoViv, FlagAtra, FlagAdj
+# Var continuas: Edad, CantCred, Desem, SalVigSol, Monto_Tasaciòn, DiasEfec, MaxDeuda, MaxDiasAtra, MaxGramOro, AntiDias
+# Var categòricas: Sexo, EstaCiv, TipoViv, FlagAtra, FlagAdj
 # ---> consultar si los flag son una categòrica
 
 
@@ -104,7 +183,7 @@ plot_missing(ds)
 #### DISTRIBUCION DE VARIABLES CONTINUAS ####
 #############################################
 
-par(mfrow = c(2,5))
+par(mfrow = c(2, 5))
 hist(ds$Edad, main = "Edad del cliente", col = "orange")
 hist(ds$CantCred, main = "Cantidad de Crèdito", col = "orange")
 hist(ds$Desem, main = "Desembolso", col = "orange") # posibles datos atìpicos
@@ -112,41 +191,28 @@ hist(ds$SalVigSol, main = "Saldo Vigente", col = "orange") # posibles datos atì
 hist(ds$Monto_Tasación, main = "Monto Tasaciòn", col = "orange") # posibles datos atìpicos
 hist(ds$DiasEfec, main = "Dias Efect", col = "orange")
 hist(ds$MaxDeuda, main = "Max Deuda", col = "orange")
-hist(ds$MaxDiasAtra, main = "Max Dìas de Atraso", col = "orange") 
+hist(ds$MaxDiasAtra, main = "Max Dìas de Atraso", col = "orange")
 hist(ds$MaxGramOro, main = "Max Gramos de Oro", col = "orange") # posibles datos atìpicos
 hist(ds$AntiDias, main = "Anti Dìas", col = "orange")
-par(mfrow = c(1,1))
- 
+par(mfrow = c(1, 1))
+
 boxplot(ds$Desem, main = "Desembolso")
 boxplot(ds$SalVigSol, main = "Saldo Vigente")
 boxplot(ds$Monto_Tasación, main = "Monto Tasaciòn")
 boxplot(ds$MaxGramOro, main = "Max Gramos de Oro")
-
-par(mfrow = c(1,2))
 boxplot(ds$Edad, main = "Edad del cliente")
-stripchart(ds$Edad, main = "Edad")
 
 
-par(mfrow = c(1,2))
-hist(ds$FlagAtra, main = "FlagAtra", col = "orange") 
-hist(ds$FlagAdj, main = "FlagAdj", col = "orange") 
+par(mfrow = c(1, 2))
+hist(ds$FlagAtra, main = "FlagAtra", col = "orange")
+hist(ds$FlagAdj, main = "FlagAdj", col = "orange")
 
 ###############################################
 #### DISTRIBUCION DE VARIABLES CATEGORICAS ####
 ###############################################
 
-par(mfrow = c(1,3))
+par(mfrow = c(1, 3))
 plot(ds$Sexo, main = "Sexo", col = "seashell")
 plot(ds$EstaCiv, main = "Estado Civil", col = "seashell")
-plot(ds$TipoViv, main = "Tipo Vivienda", col = "seashell") 
-par(mfrow = c(1,1))
-
-
-
-
-
-
-
- 
-
-
+plot(ds$TipoViv, main = "Tipo Vivienda", col = "seashell")
+par(mfrow = c(1, 1))
